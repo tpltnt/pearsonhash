790c15b5db64d9776b5b2dea6def292f5050a214
tpltnt <tpltnt.github@dropcut.net>
2013-08-31 20:12:36 +0200

    header (table) started

---
+++ pearsonhash.h
@@ -0,0 +1,30 @@
+/*
+ * pearsonhash.h
+ *
+ *  Created on: Jul 30, 2013
+ *      Author: tpltnt <tpltnt.github@dropcut.net>
+ *
+ *  This file is part of a Pearson hashing implemenation.
+ *
+ *  pearsonhash.h is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published
+ *  by the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  pearsonhash.h is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with pearsonhash.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef PEARSONHASH_H_
+#define PEARSONHASH_H_
+
+uint8_t pearsonhash_table[] = {
+  39, 159
+};
+
+uint8_t pearson_hash(uint8_t* data);
+#endif
